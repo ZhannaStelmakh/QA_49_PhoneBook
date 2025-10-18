b79fbd828a236fe214410bc8f7cced613ae8f4eb
ZhannaStelmakh <vertebrology.center@gmail.com>
2025-10-18 17:43:04 +0300

    create first test

---
+++ src/test/java/ui_tests/HomeTests.java
@@ -0,0 +1,14 @@
+package ui_tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+
+public class HomeTests extends ApplicationManager {
+
+    @Test
+    public void firstTest(){
+        System.out.println("first test");
+    }
+
+
+}
